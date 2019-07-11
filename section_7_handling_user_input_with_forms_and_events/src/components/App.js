28bc684dcebedfe144011875fb0c481f35774294
Kyle McCloskey <mccloskeykyle@gmail.com>
2019-07-11 06:01:31 -0400

    back in on section 7+

--- section_7_handling_user_input_with_forms_and_events/src/components/App.js
+++ section_7_handling_user_input_with_forms_and_events/src/components/App.js
@@ -1,15 +1,27 @@
 import React from "react";
+import axios from "axios";
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
-  onSearchSubmit(term) {
-    console.log(term);
-  }
+  state = { images: [] };
+
+  onSearchSubmit = async term => {
+    const response = await axios.get("https://api.unsplash.com/search/photos", {
+      params: { query: term },
+      headers: {
+        Authorization:
+          "Client-ID 5cb76d3901a2681fddd9c9fbe0faa3906fd3711ea52b24b52be7b35552742b8d"
+      }
+    });
+
+    this.setState({ images: response.data.results });
+  };
 
   render() {
     return (
       <div className="ui container" style={{ marginTop: "10px" }}>
         <SearchBar onSubmit={this.onSearchSubmit} />
+        Found: {this.state.images.length} images
       </div>
     );
   }
